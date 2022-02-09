9ca1f736ae5d8d72f979b4a2cc64cfbbfad63906
OhNaNaSun <k626541201@hotmail.com>
2022-02-10 00:13:38 +0800

    Test nextjs api: rename file

--- pages/api/ytvideo.ts
+++ pages/api/ytvideo.ts
@@ -7,10 +7,6 @@ export default async function (_: NextApiRequest, res: NextApiResponse) {
     'https://www.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=50&playlistId=PL3NGRQjfpiMNBKxKq-O8WcM0FLVO4GYP3&key=AIzaSyD2eP8D5vGdzoWKJX8CwpR5gOKUhVsnimk';
   // 'https://graph.instagram.com/v12.0/4754435231330979/media?fields=media_url&access_token=IGQVJYdTlJQjFKcUpIYWc5c1pOa3l2aXpaSkI4SXZAnY2hpdS00VC0wQlk1RkdOajhaNGhmc1hOaHdDS2FsYjlsdWY5RmI1SVFOazV2NkdHY3FzTEZAuUThJYzVnWWJicmVkY0F5bkl6elM0eUhfcjFNOAZDZD';
   const response = await axios.get(URL);
-  console.log(response);
-  // const data = response.data.sort(
-  //   (a: { pushed_at: string }, b: { pushed_at: string }) =>
-  //     new Date(b.pushed_at).valueOf() - new Date(a.pushed_at).valueOf()
-  // );
-  res.status(200).json(response.data);
+  const videoList = response.data.items.slice(0, 5).map((i) => i.snippet);
+  res.status(200).json(videoList);
 }
